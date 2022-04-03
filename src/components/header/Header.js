c8baf2a18d600f79d4b1c93487189600cd7466e1
samiulbasirfahim <samiulbasirfahim360@hotmail.com>
2022-04-04 00:09:29 +0600

    header done

---
+++ src/components/header/Header.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { NavLink } from "react-router-dom"
+
+const Header = () => {
+	return (
+		<div>
+			<nav>
+				<ul className="flex justify-center mt-6">
+					<li >
+						<NavLink  className="mx-2 px-5 font-semibold text-xl" style={({isActive}) => {
+                            return isActive ? {color: '#EE3848', borderBottom: "3px solid #EE3848", paddingBottom:'10px'} : {borderBottom: "3px solid #F8E9E5", paddingBottom:'5px'}
+                        }} to='/'>HOME</NavLink>
+					</li>
+					<li >
+						<NavLink  className="mx-2 px-5 font-semibold text-xl" style={({isActive}) => {
+                            return isActive ? {color: '#EE3848', borderBottom: "3px solid #EE3848", paddingBottom:'10px'} : {borderBottom: "3px solid #F8E9E5", paddingBottom:'5px'}
+                        }} to='/reviews'>REVIEWS</NavLink>
+					</li>
+					<li >
+						<NavLink  className="mx-2 px-5 font-semibold text-xl" style={({isActive}) => {
+                            return isActive ? {color: '#EE3848', borderBottom: "3px solid #EE3848", paddingBottom:'10px'} : {borderBottom: "3px solid #F8E9E5", paddingBottom:'5px'}
+                        }} to="/dashboard">DASHBOARD</NavLink>
+					</li>
+					<li >
+						<NavLink  className="mx-2 px-5 font-semibold text-xl" style={({isActive}) => {
+                            return isActive ? {color: '#EE3848', borderBottom: "3px solid #EE3848", paddingBottom:'10px'} : {borderBottom: "3px solid #F8E9E5", paddingBottom:'5px'}
+                        }} to="/blogs">BLOGS</NavLink>
+					</li>
+					<li >
+						<NavLink  className="mx-2 px-5 font-semibold text-xl" style={({isActive}) => {
+                            return isActive ? {color: '#EE3848', borderBottom: "3px solid #EE3848", paddingBottom:'10px'} : {borderBottom: "3px solid #F8E9E5", paddingBottom:'5px'}
+                        }} to="/about">ABOUT</NavLink>
+					</li>
+				</ul>
+			</nav>
+		</div>
+	)
+}
+
+export default Header
